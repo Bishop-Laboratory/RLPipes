5451e4d08349a6bb9ddac28ee680aa03068596c5
millerh1 <millerh1@livemail.uthscsa.edu>
2021-05-28 15:07:15 -0500

    Set up CI for install and testing in github

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 testBasicDryDRIP() {
-  bash ../bin/RSeq -e SRX1025890_TC32_NT_DRIP.hg38.bam -g hg38 -m DRIP -S dryrun=True -o testBasicDryDRIP
+  RSeq -e SRX1025890_TC32_NT_DRIP.hg38.bam -g hg38 -m DRIP -S dryrun=True -o testBasicDryDRIP
 }
 
 testBasicDRIP() {
-  bash ../bin/RSeq -e SRX1025890_TC32_NT_DRIP.hg38.bam -g hg38 -m DRIP -o testBasicDRIP
+  RSeq -e SRX1025890_TC32_NT_DRIP.hg38.bam -g hg38 -m DRIP -o testBasicDRIP
 }
 
 testInputDRIP() {
-  bash ../bin/RSeq -e SRX1025890_TC32_NT_DRIP.hg38.bam -g hg38 -m DRIP -c SRX1025893_TC32_Input.hg38.bam -o testInputDRIP
+  RSeq -e SRX1025890_TC32_NT_DRIP.hg38.bam -g hg38 -m DRIP -c SRX1025893_TC32_Input.hg38.bam -o testInputDRIP
 }
 
 # Load shUnit2.
