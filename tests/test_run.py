14c7f58a9722c8885cf8d65cc9503e030c2496f8
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-29 13:31:54 -0500

    minor

--- tests/test_run.py
+++ tests/test_run.py
@@ -7,5 +7,5 @@ def test_check():
   runner = CliRunner()
   buildres = runner.invoke(build, ['rseq_out_bams/', 'test_data/bam_test_samples_1.csv'])
   checkres = runner.invoke(check, ['rseq_out_bams/'])
-  runres = runner.invoke(run, ['rseq_out_bams/'])
+  # runres = runner.invoke(run, ['rseq_out_bams/'])
   assert checkres.exit_code == 0
