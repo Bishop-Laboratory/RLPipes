7d3c73f865ae4202242c8ee9bf6bf4a032e842fc
millerh1 <millerh1@livemail.uthscsa.edu>
2022-08-05 13:32:16 -0500

    Remove run test

--- tests/test_run.py
+++ tests/test_run.py
@@ -12,16 +12,16 @@ FQSAMPSGZ='tests/test_data/fq_test_samples_1.csv'
 FQSAMPSGZ2='tests/test_data/fq_test_samples_4.csv'
 
 
-def test_run_1():
-  if os.path.exists(RSEQ_OUT_BAM1):
-    shutil.rmtree(RSEQ_OUT_BAM1)
-  runner = CliRunner()
-  buildres = runner.invoke(build, [RSEQ_OUT_BAM1, BAMSAMPS])
-  checkres = runner.invoke(check, [RSEQ_OUT_BAM1])
-  runres = runner.invoke(run, [RSEQ_OUT_BAM1, "--debug", "-t", "44"])
-  assert runres.exit_code == 0
-
+# def test_run_1():
+#   if os.path.exists(RSEQ_OUT_BAM1):
+#     shutil.rmtree(RSEQ_OUT_BAM1)
+#   runner = CliRunner()
+#   buildres = runner.invoke(build, [RSEQ_OUT_BAM1, BAMSAMPS])
+#   checkres = runner.invoke(check, [RSEQ_OUT_BAM1])
+#   runres = runner.invoke(run, [RSEQ_OUT_BAM1, "--debug", "-t", "44"])
+#   assert runres.exit_code == 0
 # 
+# # 
 # def test_run_2():
 #   if os.path.exists(RSEQ_OUT_FQ2):
 #     shutil.rmtree(RSEQ_OUT_FQ2)
